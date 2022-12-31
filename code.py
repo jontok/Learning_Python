8b4bfb9cf351c7ad4fe03e1e93a3c54e50216e3b
jontok <jonas@tokmaji.de>
2022-12-31 02:28:28 +0100

    refactoring and fixes

--- code.py
+++ code.py
@@ -1,38 +1,50 @@
 import json
 import random
 
+
+
 string = "schokolade"
+
+numbers_x = [10, 20, 30, 40, 10]
+numbers_y = [75, 65, 35, 75, 30]
+
+my_str = "Spiderman is not even a real spider"
+sub_str = "spider"
+
+list1 = [10, 20, 25, 30, 35]
+list2 = [40, 45, 60, 75, 90]
+
+
 
 def print_Even_Number():
 	# your code
     c = 0
     for i in string:
-        if (c % 2):
+        if (c % 2 == 0):
             print(i)
         c = c +1 
         
 # print_Even_Number()
 
-numbers_x = [10, 20, 30, 40, 10]
-numbers_y = [75, 65, 35, 75, 30]
+
 
 def last_same_as_first(numbers):
     # your code
     if numbers[0] == numbers[len(numbers)-1]:
         print("True")
+    else:
+        print("False")
 
 #last_same_as_first(numbers_y)
 
-my_str = "Spiderman is not even a real spider"
-sub_str = "spider"
+
 def spider_counter(my_str, sub_str):
     my_str = my_str.lower()
     return my_str.count(sub_str)
  
 # print(spider_counter(my_str, sub_str))
 
-list1 = [10, 20, 25, 30, 35]
-list2 = [40, 45, 60, 75, 90]
+
 
 def combine_lists(list1,list2):
     list3 = []
--- yahoo_users.py
+++ yahoo_users.py
@@ -1,19 +1,22 @@
 import csv
 
+def counting_users(table: str,line_count: int,provider: str,c: int):
+    for row in table:
+        line_count = line_count+1    
+        if provider in row[4]:
+            c = c+1
+    return [line_count, c]        
 
-
-def who_uses_yahoo(provider):
+def who_uses_yahoo(provider: str):
     with open('table.csv') as csv_file:
         table = csv.reader(csv_file, delimiter=',')
         line_count = 0
         c = 0
-        for row in table:
-            line_count = line_count+1    
-            if provider in row[4]:
-                    c = c+1                
+        counters = counting_users(table,line_count,provider,c)
+        line_count = counters[0]
+        c = counters[1]
 
-        
         percentage = round(100 * c/line_count, 2)
         return percentage
 
-print(who_uses_yahoo("yahoo.com"))
+# print(who_uses_yahoo("yahoo.com")) 
